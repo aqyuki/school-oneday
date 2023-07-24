a4566c0111e1c18d299af86971bd6b91c18f7ba0
K.Yosuke <yksk171118@gmail.com>
2023-07-24 17:47:38 +0900

    :zap: フッターを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import OneDayFooter from "@/components/footer";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'タイトル',
-  description: '鹿児島高専e-スポーツクラブの活動様子など',
-}
+  title: "タイトル",
+  description: "鹿児島高専e-スポーツクラブの活動様子など",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" data-theme="light">
-      <body className={inter.className}> {children}</body>
+      <body className={inter.className}>
+        {children}
+        <OneDayFooter />
+      </body>
     </html>
-  )
+  );
 }
