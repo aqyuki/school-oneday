a77dc2fef5e2f44a68eaa45e235418cc2d18e057
K.Yosuke <yksk171118@gmail.com>
2023-07-27 15:28:43 +0900

    :memo: 記事を更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import { LeftImageArticles, RightImageArticles } from "@/components/articles";
+import {
+  LeftImageArticles,
+  NonImageArticles,
+  RightImageArticles,
+} from "@/components/articles";
 
 export default function Home() {
   return (
@@ -11,15 +15,20 @@ export default function Home() {
         title="文化祭"
         alt="文化祭の様子"
       >
-        「音ゲー部門」と「格ゲー部門」でそれぞれ体験会を開いています。誰でも自由に参加する事ができます。今年もやりますので、ぜひ遊びに来てください！
+        「音ゲー部門」と「格ゲー部門」でそれぞれ体験会を開いています。誰でも自由に参加することができます。写真は「Street
+        Fighter
+        V」という格闘ゲームの対戦の体験です。昨年は小学生から学生、大人まで多くの人が体験してくれました。今年は両方の部門ともさらにグレードアップして、体験会に加えそれぞれ楽しい企画を計画しています。ぜひ遊びに来てください！
       </RightImageArticles>
       <LeftImageArticles
         image="/images/class_match.jpg"
         title="クラスマッチ"
         alt="クラスマッチの様子"
       >
-        「太鼓の達人」の「ふつう・むずかしい」「おに」部門でそれぞれ腕を競います。年々参加者も観戦者も増え、規模が拡大しています。
+        クラスマッチに「太鼓の達人」という音楽ゲームの部門を加え企画運営を始めてから今年で3年目となります。「ふつう・むずかしい」「おに」部門でそれぞれ腕を競います。年々参加者も観戦者も増えており、規模が拡大しています。楽しく参加できることは勿論、他の参加者の素晴らしいプレイを見たり大会専用のプレゼンや創作譜面でのプレイを見たりすることも魅力です。
       </LeftImageArticles>
+      <NonImageArticles title="その他">
+        普段は各種目での練習会や、コントローラーのメンテナンス、大会準備、勉強会、様々な講習会などの活動を行っております。ランキングやe-sports大会への参加も計画実行しています。
+      </NonImageArticles>
     </main>
   );
 }
