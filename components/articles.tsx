08ff97940003d61f853b18322b20e7b7cb44e40c
K.Yosuke <yksk171118@gmail.com>
2023-07-27 15:29:06 +0900

    :sparkles: 画像なしの記事用のコンポーネントを作成

--- components/articles.tsx
+++ components/articles.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { memo } from "react";
-import ArticlesPropsType, { ArticlesContentPropsType } from "../types/articles";
+import ArticlesPropsType, {
+  ArticlesContentPropsType,
+  NonImageArticlesPropsType,
+} from "../types/articles";
 
 export const RightImageArticles = memo((props: ArticlesPropsType) => {
   return (
-    <div className="hero min-h-screen h-50">
+    <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row w-3/5">
         <Image
           src={props.image}
@@ -27,7 +30,7 @@ export const RightImageArticles = memo((props: ArticlesPropsType) => {
 
 export const LeftImageArticles = memo((props: ArticlesPropsType) => {
   return (
-    <div className="hero min-h-screen h-50">
+    <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse w-3/5">
         <Image
           src={props.image}
@@ -47,10 +50,26 @@ export const LeftImageArticles = memo((props: ArticlesPropsType) => {
   );
 });
 
+export const NonImageArticles = memo((props: NonImageArticlesPropsType) => {
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse w-3/5">
+        <div>
+          <h1 className="text-4xl font-bold md:text-start sm:text-center">
+            {props.title}
+          </h1>
+          <ArticlesContent>{props.children}</ArticlesContent>
+        </div>
+      </div>
+    </div>
+  );
+});
+
 const ArticlesContent = ({ children }: ArticlesContentPropsType) => {
   return <p className="py-7 text-2xl md:w-full w-screen">{children}</p>;
 };
 
 RightImageArticles.displayName = "RightImageArticles";
 LeftImageArticles.displayName = "LeftImageArticles";
+NonImageArticles.displayName = "NonImageArticles";
 export default LeftImageArticles;
